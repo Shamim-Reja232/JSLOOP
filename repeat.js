55ddea02c009509de3bc29cafa7c4fc4f78b9a22
Shamim Reja <your_email@example.com>
2026-07-19 12:19:51 +0600

    print all odd number from 1 to 100

--- repeat.js
+++ repeat.js
@@ -13,8 +13,14 @@
 //}
 //***************************** */
 //Print numbers from 10 to 1 using a for loop.
-for (let i = 10 ; i>1; i--){
-    console.log(i);
+//for (let i = 10 ; i>1; i--){
+     //  console.log(i);
+//}
+//Print all odd numbers from 1 to 100.
+for (let i =1; i <=100; i++){
+    if (i % 2 ==0){
+        console.log(i);
+    }
 }
 
 
