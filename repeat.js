bf0d1cfe01e611b38d3fa9afcb398311d2acf127
Shamim Reja <your_email@example.com>
2026-07-19 12:02:23 +0600

    print numbers frome 1 to 10 using  a for loop

--- repeat.js
+++ repeat.js
@@ -2,6 +2,13 @@
 // repetitive taks}
 
 
-for (let count = 0; count <10 ; count++){
-    console.log('I Love You ');
+//for (let count = 0; count <10 ; count++){
+  //  console.log('I Love You ');
+//}
+
+//* Print numbers from 1 to 10 using a for loop.
+
+for (let i =1; i<=10 ; i++){
+         console.log(i);
 }
+
